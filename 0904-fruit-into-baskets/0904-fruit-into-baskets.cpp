a69086f70772b1134e734dd9d4331da0cbde3629
Eshiv <eshivpandey18@gmail.com>
2026-08-26 20:42:04 +0530

    Time: 47 ms (43.37%), Space: 85 MB (34.47%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
@@ -5,12 +5,10 @@ public:
         int l=0,r=0,maxi=1;
         while(r<fruits.size()){
             mp[fruits[r]]++;
-            if(mp.size()>2){
-                while(mp.size()>2){
+            while(mp.size()>2){
                     mp[fruits[l]]--;
                     if(mp[fruits[l]]==0) mp.erase(fruits[l]);
                     l++;
-                }
             }maxi=max(maxi,r-l+1);
             r++;
         }return maxi;
